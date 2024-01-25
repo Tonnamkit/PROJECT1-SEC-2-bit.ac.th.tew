2ed3a2d93020a9276e0d7eb7c15cb8d52b7810cf
BomScoob <sorawitkraukham@gmail.com>
2024-01-25 20:32:51 +0700

    implement game logic { checkAnswer, isGameOver, etc. }

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,33 @@ import Questions from "../data/question"
 
 const gameStarted = ref(false);
 const gameEnded = ref(false);
+const gameOver = ref(false);
 const lifePoints = ref(3);
 const score = ref(0);
 const quizes = reactive(Questions);
 const currentQuiz = ref(0);
+
+const isCorrectAnswer = (optionId) => {
+  if (optionId === answerIndex) {
+    setBtnStyle("green")
+    score.value++;
+  } else {
+    setBtnStyle("red")
+    --lifePoints.value;
+    if (lifePoints.value === 0) {
+      gameOver.value = true;
+    }
+  }
+  if (!isGameEnd(currentQuiz.value)) {
+    currentQuiz.value++;
+  } else {
+    gameEnded.value = true;
+  }
+}
+
+const isGameEnd = (quizIndex) => {
+  return quizIndex === (quizes.length-1)
+}
 </script>
 
 <template>
