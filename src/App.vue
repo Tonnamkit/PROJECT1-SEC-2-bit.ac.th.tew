33b2fe2a5b3a18b710b4a8989efe210f55dc1b66
BomScoob <sorawitkraukham@gmail.com>
2024-01-31 13:48:24 +0700

    test state

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { ref, reactive } from "vue"
-import Questions from "../data/question"
+import { ref, reactive } from 'vue'
+import Questions from '../data/question'
 
 const quizzes = reactive(Questions)
 
@@ -38,15 +38,27 @@ const useGameStore = (lifePoints) => {
     },
     restart() {
       this.reset()
-      state.gameStarted = true;
-    }
-  };
+      state.gameStarted = true
+    },
   }
-
-  return { state, actions }
 }
 
-const { state, actions } = useGameStore(3);
+return { state, actions }
+
+const { state, actions } = useGameStore(3)
+console.log(state)
+console.log(state.lifePoints)
+console.log('current : ' + state.currentQuiz)
+console.log('current ++ :' + ++state.currentQuiz)
+console.log(state.gameEnded)
+
+//? how to access object question
+console.log(quizzes[0].question)
+
+const isCorrectAnswer = (optionAns) => {
+  if (optionAns === quizzes[state.currentQuiz].answer) {
+  }
+}
 
 //bomscope
 
@@ -75,12 +87,25 @@ const { state, actions } = useGameStore(3);
 
 <template>
   <div class="h-screen w-screen flex items-center">
-    <link rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+    />
     <!-- Main Menu -->
-    <div id="main-menu" v-if="!state.gameStarted" class="flex flex-col justify-center items-center gap-[15%] sm:gap-[22%] h-1/2 sm:w-1/2 mx-auto text-center">
-      <h1 class="text-4xl sm:text-5xl font-bold leading-loose">Funny Quiz Game</h1>
-      <button @click="actions.startGame" class="btn btn-outline w-3/5 sm:w-1/2 text-xl">Start Game</button>
+    <div
+      id="main-menu"
+      v-if="!state.gameStarted"
+      class="flex flex-col justify-center items-center gap-[15%] sm:gap-[22%] h-1/2 sm:w-1/2 mx-auto text-center"
+    >
+      <h1 class="text-4xl sm:text-5xl font-bold leading-loose">
+        Funny Quiz Game
+      </h1>
+      <button
+        @click="actions.startGame"
+        class="btn btn-outline w-3/5 sm:w-1/2 text-xl"
+      >
+        Start Game
+      </button>
     </div>
     <!-- Quiz -->
     <div id="quiz-section" v-else>
@@ -103,9 +128,12 @@ const { state, actions } = useGameStore(3);
       </div>
     </div>
     <!-- Result Overlay -->
-    <div id="result" v-show="state.gameEnded"
-      class="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-75 flex items-center justify-center z-50">
-      <div id="result-content" class="bg-white p-8 rounded-lg text-center"> 
+    <div
+      id="result"
+      v-show="state.gameEnded"
+      class="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-75 flex items-center justify-center z-50"
+    >
+      <div id="result-content" class="bg-white p-8 rounded-lg text-center">
         <div id="header-section">
           <h2 class="text-5xl font-bold">Game Ended</h2>
         </div>
@@ -116,18 +144,21 @@ const { state, actions } = useGameStore(3);
           <h3>Under Construction!!</h3>
         </div>
         <div id="btn-section" class="flex justify-center">
-          <button @click="actions.reset" class="text-xl px-4 py-2 bg-blue-500 text-white rounded mr-4"><span
-              class="material-symbols-outlined">
-              home
-            </span></button>
-          <button @click="actions.restart" class="text-xl px-4 py-2 bg-green-500 text-white rounded"><span
-              class="material-symbols-outlined">
-              restart_alt
-            </span></button>
+          <button
+            @click="actions.reset"
+            class="text-xl px-4 py-2 bg-blue-500 text-white rounded mr-4"
+          >
+            <span class="material-symbols-outlined"> home </span>
+          </button>
+          <button
+            @click="actions.restart"
+            class="text-xl px-4 py-2 bg-green-500 text-white rounded"
+          >
+            <span class="material-symbols-outlined"> restart_alt </span>
+          </button>
         </div>
       </div>
     </div>
-
   </div>
 </template>
 
