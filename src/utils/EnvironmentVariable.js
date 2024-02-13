f8fa8ad796f256ae107646c66f30ee4c01d59a1e
BomScoob <sorawitkraukham@gmail.com>
2024-02-13 22:30:12 +0700

    update : environment variable style;

--- src/utils/EnvironmentVariable.js
+++ src/utils/EnvironmentVariable.js
@@ -1,23 +1,22 @@
 const INITIAL_LIFE_POINTS = 3;
 const btnStyleCss =
-  'border-4 border-none bg-golden-sand-50 rounded-3xl flex justify-center items-center shadow-lg text-color-default text-xl md:text-3xl p-4 md:p-8 w-full md:w-2/5 hover:bg-golden-sand-100 transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-105 duration-300 h-fit';
+  'border-4 border-none bg-downriver-50 rounded-3xl flex justify-center items-center shadow-lg text-color-default text-xl md:text-2xl p-2 md:py-6 w-full md:w-2/5 hover:bg-downriver-100 transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-105 duration-300 h-fit';
 const textBoxStyleCss =
-  'input input-bordered input-lg w-full text-color-default text-xl md:text-3xl p-4 md:p-8 hover:bg-golden-sand-100';
-
+  'input input-bordered input-lg w-full border-4 border-none bg-downriver-50 rounded-3xl flex justify-center items-center shadow-lg text-color-default text-xl md:text-2xl p-2 md:py-6 w-full md:w-2/5 hover:bg-downriver-100 pl-4';
 const addOns = {
   dropExtraLifeRatio: 5,
 };
 
 const buttonStyles = Object.freeze({
   DEFAULT: btnStyleCss,
-  CORRECT: btnStyleCss + ' bg-green-700 hover:bg-green-800 text-white',
-  INCORRECT: btnStyleCss + ' bg-red-700 hover:bg-red-800 text-white',
+  CORRECT: btnStyleCss + ' bg-green-600 hover:bg-green-700 text-white',
+  INCORRECT: btnStyleCss + ' bg-red-600 hover:bg-red-700 text-white',
 });
 
 const textBoxStyles = Object.freeze({
   DEFAULT: textBoxStyleCss,
-  CORRECT: textBoxStyleCss + ' bg-green-700 hover:bg-green-800 text-white',
-  INCORRECT: textBoxStyleCss + ' bg-red-700 hover:bg-red-800 text-white',
+  CORRECT: textBoxStyleCss + ' bg-green-600 hover:bg-green-700 text-white',
+  INCORRECT: textBoxStyleCss + ' bg-red-600 hover:bg-red-700 text-white',
 });
 
 const expectedRangesWithMessages = [
